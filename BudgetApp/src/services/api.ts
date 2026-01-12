65c60f29592edade06aa4628f37508a1da240bd6
LakshanDerren <isuru21256@gmail.com>
2026-01-13 00:11:28 +0530

    Enable HTTPS

--- BudgetApp/src/services/api.ts
+++ BudgetApp/src/services/api.ts
@@ -3,7 +3,7 @@ import { Platform } from 'react-native';
 import storage from '../store/storage';
 
 // Production EC2 URL
-const DEV_URL = 'http://51.21.222.39:5000';
+const DEV_URL = 'https://dailybudget.duckdns.org';
 
 const api = axios.create({
     baseURL: process.env.EXPO_PUBLIC_API_URL || DEV_URL,
